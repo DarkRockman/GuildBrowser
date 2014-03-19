103c302b95942e4f29239d5e353b7eca10f7af3d
DarkRockman <digitalandrade1@gmail.com>
2014-03-19 13:04:21 -0500

    Created test cases for character

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Michael Andrade on 19/03/14.
+//  Copyright (c) 2014 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
